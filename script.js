47d136fa18a61dadea8c1cb971cfb42254493177
saime12 <ohtradaniel@gmail.com>
2026-02-26 22:47:03 +0200

    new

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 const addBtn = document.getElementById('button')
 
 if(addBtn) {
-addBtn.addEventListener('click', function() {
-    window.location.replace("add.html")
-})
+    addBtn.addEventListener('click', function() {
+        window.location.replace("add.html")
+    })
 }
 
 const postBtn = document.getElementById('postBtn')
 
 if(postBtn) {
-postBtn.addEventListener('click', async function() {
-    window.location.replace("index.html")
-    postData()
-})
+    postBtn.addEventListener('click', async function() {
+        if(title.value === "", price.value === "", description.value === "", image.value === "") {
+            alert("fill inputs")
+        }
+        else {
+            window.location.replace("index.html")
+            postData()
+        }    
+    })
 }
 
 const title = document.getElementById('title')
 const price = document.getElementById('price')
 const description = document.getElementById('description')
 const image = document.getElementById('image')
-let oldData = []
 
 async function postData() {
     const response = await fetch('https://tinkr.tech/sdb/db_newMarket', {
@@ -29,21 +33,24 @@ async function postData() {
         user: 'John',
         title: title.value ,
         price: price.value,
-        sold: false,
         description: description.value ,
         image: image.value
     })
     });
     const data = await response.json();
-    console.log(data);
+    console.log(data)
 }
 
 async function getData() {
     const response = await fetch('https://tinkr.tech/sdb/db_newMarket');
-    const documents = await response.json();
-    console.log(documents);
+    const data = await response.json();
+    return data
 }
-getData()
 
 const listingsDiv = document.getElementById('listings')
 
+async function addToPage() {
+    const data = await getData()
+    console.log(data[3].price)
+}
+addToPage()
